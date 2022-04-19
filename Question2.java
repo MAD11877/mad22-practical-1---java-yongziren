62df9fd2f287708f610a1cc302a0f443a81c9839
New_President <zirenyong2004@gmail.com>
2022-04-20 00:40:36 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -10,7 +10,11 @@ public class Question2
      * Hint: in.nextDouble() to read double
      */
      
-    Scanner in = new Scanner(System.in);
+    System.out.print("Input weight in kilogram: ");
+    double weight = in.nextDouble();
+    System.out.print("Input height in meters: ");
     double height = in.nextDouble();
+    double BMI = weight / (height * height);
+    System.out.print(BMI);
   }
 }
